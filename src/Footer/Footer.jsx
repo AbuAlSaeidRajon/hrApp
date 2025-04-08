a60ff2cdb5f8dab248ece453021e07acfa6027cc
Md Abu Al Saeid <khanrajon5@gmail.com>
2025-04-08 09:36:45 +0300

    style change

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>Md Saeid &copy; 2025 REACT25K</p>
+      <p>Copyright &copy; Md Saeid 2025 REACT25K</p>
     </footer>
   );
 };
